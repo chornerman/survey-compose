1f51f6c8676748d2463b2508dc6f6c659cac8a35
Chorn <kritchai.kus42@gmail.com>
2023-02-09 09:58:23 +0700

    [#3] Rename secret

--- .github/workflows/decrypt_secret.sh
+++ .github/workflows/decrypt_secret.sh
@@ -4,5 +4,5 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
   --output app/google-services.json .github/workflows/secrets/google-services.json.gpg
